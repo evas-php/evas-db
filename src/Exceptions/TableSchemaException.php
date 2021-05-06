466eab5b6d0d0fb7e6f6b894405a3f627d06fc4a
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-05-06 03:35:04 +0300

    Create TableSchemaException.php

---
+++ src/Exceptions/TableSchemaException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Класс исключения схемы таблицы.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Exceptions;
+
+use Evas\Db\Exceptions\DbException;
+
+class TableSchemaException extends DbException
+{}
