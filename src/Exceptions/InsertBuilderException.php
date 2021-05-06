50e49b2a5b25510463e23964c31f9d3a9a8340a3
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-05-06 03:34:15 +0300

    Create InsertBuilderException.php

---
+++ src/Exceptions/InsertBuilderException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Класс исключения сборщика INSERT-запросов.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Exceptions;
+
+use Evas\Db\Exceptions\DbException;
+
+class InsertBuilderException extends DbException
+{}
