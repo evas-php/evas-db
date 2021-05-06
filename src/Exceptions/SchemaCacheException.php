68d0839cd707470a23e8875e059427c97b335f49
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-05-06 03:35:07 +0300

    Create SchemaCacheException.php

---
+++ src/Exceptions/SchemaCacheException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Класс исключения кеша схемы баз данных.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Exceptions;
+
+use Evas\Db\Exceptions\DbException;
+
+class SchemaCacheException extends DbException
+{}
