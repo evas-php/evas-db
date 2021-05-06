f75d01323dab29765d78ba3adecaf0060558b861
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-05-06 03:32:54 +0300

    Create DbException.php

---
+++ src/Exceptions/DbException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Класс исключений модуля evas-db.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Exceptions;
+
+class DbException extends \Exception
+{}
