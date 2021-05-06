361655dcbc62526f357df3b85f79aac7c439d8b2
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-05-06 03:33:13 +0300

    Create QueryResult.php

---
+++ src/Base/QueryResult.php
@@ -0,0 +1,244 @@
+<?php
+/**
+ * Класс-обертка над PDOStatement для получения ответа запроса в удобном виде.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Base;
+
+use \PDO;
+use \PDOStatement;
+use Evas\Db\Interfaces\DatabaseInterface;
+use Evas\Db\Interfaces\QueryResultInterface;
+
+class QueryResult implements QueryResultInterface
+{
+    /** @var PDOStatement */
+    protected $stmt;
+
+    /** @var DatabaseInterface */
+    protected $db;
+
+    /**
+     * Конструктор.
+     * @param PDOStatement
+     * @param DatabaseInterface
+     */
+    public function __construct(PDOStatement &$stmt, DatabaseInterface &$db)
+    {
+        $this->stmt = &$stmt;
+        $this->db = &$db;
+    }
+
+    /**
+     * Деструтор.
+     */
+    public function __destruct()
+    {
+        $this->stmt()->closeCursor();
+    }
+
+    /**
+     * Получение statement ответа базы.
+     * @return PDOStatement
+     */
+    public function stmt(): PDOStatement
+    {
+        return $this->stmt;
+    }
+
+    /**
+     * Получение имени таблицы для select-запроса.
+     * @return string|null
+     */
+    public function tableName(): ?string
+    {
+        if (0 > $this->stmt->columnCount()) return null;
+        $columnMeta = $this->stmt->getColumnMeta(0);
+        return $columnMeta['table'] ?? null;
+    }
+
+    /**
+     * Получение количества возвращённых строк.
+     * @return int
+     */
+    public function rowCount(): int
+    {
+        return $this->stmt->rowCount();
+    }
+
+
+    // Получение записи/записей в разном виде.
+
+    /**
+     * Получение записи в виде нумерованного массива.
+     * @return numericArray|null
+     */
+    public function numericArray(): ?array
+    {
+        $row = $this->rowCount() > 0 ? $this->stmt->fetch(PDO::FETCH_NUM) : null;
+    }
+
+    /**
+     * Получение всех записей в виде массива нумерованных массивов.
+     * @return array
+     */
+    public function numericArrayAll(): array
+    {
+        return $this->rowCount() > 0 ? $this->stmt->fetchAll(PDO::FETCH_NUM) : [];
+    }
+
+    /**
+     * Получение записи в виде ассоциативного массива.
+     * @return assocArray|null
+     */
+    public function assocArray(): ?array
+    {
+        return $this->rowCount() > 0 ? $this->stmt->fetch(PDO::FETCH_ASSOC) : null;
+    }
+
+    /**
+     * Получение всех записей в виде массива ассоциативных массивов.
+     * @return array
+     */
+    public function assocArrayAll(): array
+    {
+        return $this->rowCount() > 0 ? $this->stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+    }
+
+    /**
+     * Получение записи в виде анонимного объекта.
+     * @return stdClass|null
+     */
+    public function anonymObject(): ?object
+    {
+        $row = $this->rowCount() > 0 ? $this->stmt->fetch(PDO::FETCH_OBJ) : null;
+        return $this->objectHook($row);
+    }
+
+    /**
+     * Получение всех записей в виде массива анонимных объектов.
+     * @return array
+     */
+    public function anonymObjectAll(): array
+    {
+        $rows = $this->rowCount() > 0 ? $this->stmt->fetchAll(PDO::FETCH_OBJ) : [];
+        return $this->objectsHook($rows);
+    }
+
+    /**
+     * Получение записи в виде объекта класса.
+     * @param string имя класса
+     * @return object|null
+     */
+    public function classObject(string $className): ?object
+    {
+        if (0 === $this->rowCount()) return null;
+        $this->stmt->setFetchMode(PDO::FETCH_CLASS, $className);
+        $row = $this->stmt->fetch();
+        return $this->objectHook($row);
+    }
+
+    /**
+     * Получение всех записей в виде массива объектов класса.
+     * @param string имя класса
+     * @return array
+     */
+    public function classObjectAll(string $className): array
+    {
+        if (0 === $this->rowCount()) return [];
+        $this->stmt->setFetchMode(PDO::FETCH_CLASS, $className);
+        $rows = $this->stmt->fetchAll();
+        return $this->objectsHook($rows);
+    }
+
+    /**
+     * Добавление параметров записи в объект.
+     * @param object
+     * @return object
+     */
+    public function intoObject(object &$object): object
+    {
+        if (0 === $this->rowCount()) return $object;
+        $this->stmt->setFetchMode(PDO::FETCH_INTO, $object);
+        $this->stmt->fetch();
+        return $this->objectHook($object);
+    }
+
+
+    // Хуки
+
+
+    /**
+     * Хук для постобработки полученного объекта записи.
+     * @param object|null запись
+     * @return object|null постобработанная запись
+     */
+    public function objectHook(object &$row = null): ?object
+    {
+        if (!empty($row) && is_object($row)) {
+            $this->identityMapUpdate($row);
+            $this->runEntityAfterFindMethod($row);
+        }
+        return $row;
+    }
+
+    /**
+     * Хук для постобработки полученных объектов записей.
+     * @param array|null записи
+     * @return array|null постобработанные записи
+     */
+    public function objectsHook(array &$rows = null): ?array
+    {
+        if (!empty($rows) && ($this->canIdentityMapUpdate()
+            || $this->canRunEntityAfterFindMethod($row[0])))
+            foreach ($rows as &$row) {
+                $row = $this->objectHook($row);
+            }
+        return $rows;
+    }
+
+    // Добавленные хуки
+
+    /**
+     * Проверка возможности запуска метода afterFind сущности.
+     * @param object сущность
+     * @return bool
+     */
+    public function canRunEntityAfterFindMethod(object &$row): bool
+    {
+        return method_exists($row, 'afterFind');
+    }
+
+    /**
+     * Запуска метода afterFind сущности, если есть.
+     * @param object сущность
+     */
+    public function runEntityAfterFindMethod(object &$row)
+    {
+        if ($this->canRunEntityAfterFindMethod($row)) $row->afterFind();
+    }
+
+    /**
+     * Проверка возможности обновления IdentityMap.
+     * @return bool
+     */
+    public function canIdentityMapUpdate(): bool
+    {
+        return method_exists($this->db, 'table') 
+            && method_exists($this->db, 'identityMapUpdate');
+    }
+
+    /**
+     * Хук для обновления сущности в IdentityMap.
+     * @param object сущность
+     */
+    public function identityMapUpdate(object &$object)
+    {
+        if ($this->canIdentityMapUpdate()) {
+            $table = $this->tableName();
+            $primaryKey = $this->db->table($table)->primaryKey();
+            $object = $this->db->identityMapUpdate($object, $primaryKey);
+        }
+    }
+}
