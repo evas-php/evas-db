d5385e54f83545f78547ccf793d2cad3b32ac054
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-05-06 03:35:19 +0300

    Create IdentityMapException.php

---
+++ src/Exceptions/IdentityMapException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Класс исключения маппинга идентичности сущностей IdentityMap.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Exceptions;
+
+use Evas\Db\Exceptions\DbException;
+
+class IdentityMapException extends DbException
+{}
