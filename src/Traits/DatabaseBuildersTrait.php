e219e020af4b6f41a2a4b58456990da876c95d8a
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-06-23 04:08:48 +0300

    Update DatabaseBuildersTrait.php

--- src/Traits/DatabaseBuildersTrait.php
+++ src/Traits/DatabaseBuildersTrait.php
@@ -41,10 +41,10 @@ trait DatabaseBuildersTrait
      * @param array|null столбцы записи
      * @return QueryResultInterface
      */
-    public function batchInsert(string $tbl, array $columns, array $rows = null): QueryResultInterface
+    public function batchInsert(string $tbl, array $rows, array $columns = null): QueryResultInterface
     {
         $ib = $this->insert($tbl);
-        if (!empty($rows)) $ib->keys($columns);
+        if (!empty($columns)) $ib->keys($columns);
         return $ib->rows($rows)->query();
     }
 
