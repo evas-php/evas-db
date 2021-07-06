ef8d4bb2422af119b88e00773e85e0e784deebed
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-06 04:31:42 +0300

    Create conf.mysql.php

---
+++ tests/help/conf.mysql.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Конфиг базы данных mysql для тестов.
+ */
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '***',
+    'dbname' => 'evas-tests',
+];
