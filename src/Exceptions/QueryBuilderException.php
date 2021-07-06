c77d8093b881af7294c310c0bc95a5542c026a28
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-06 04:34:40 +0300

    Create QueryBuilderException.php

---
+++ src/Exceptions/QueryBuilderException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Класс исключения сборщика запросов SELECT/UPDATE/DELETE.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Exceptions;
+
+use Evas\Db\Exceptions\DbException;
+
+class QueryBuilderException extends DbException
+{}
