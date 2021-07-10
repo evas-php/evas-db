e9e45978de5747f14711bc245924eea5041488d3
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-10 23:33:13 +0300

    Update IdentityMapException.php

--- src/Exceptions/IdentityMapException.php
+++ src/Exceptions/IdentityMapException.php
@@ -9,4 +9,20 @@ namespace Evas\Db\Exceptions;
 use Evas\Db\Exceptions\DbException;
 
 class IdentityMapException extends DbException
-{}
+{
+    /**
+     * Выбрасывание исключения IdentityMap вместе с информацией.
+     * @param string сообщение ошибки
+     * @param array информация о сущности
+     * @param string|int|null значение первичного ключа сущности
+     */
+    public static function withInfo(string $error, array $entity, $primary = null)
+    {
+        if ($primary) $entity['primaryValue'] = $primary;
+        $data = [
+            'error' => $error,
+            'entity' => $entity,
+        ];
+        return new static(json_encode($data));
+    }
+}
