60c4372e0bf571f0a0430a26e2e1b69e08491c0d
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-09-15 21:50:14 +0300

    Create DatabaseConnectionException.php

---
+++ src/Exceptions/DatabaseConnectionException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Исключение соединения с базой данных.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Exceptions;
+
+use Evas\Db\Exceptions\DbException;
+
+class DatabaseConnectionException extends DbException
+{}
