c1f66ff6ce6de095e4336d6d91c71b56f0d8fe31
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-09-19 05:36:26 +0300

    Update Database.php

--- src/Database.php
+++ src/Database.php
@@ -1,13 +1,20 @@
 <?php
+/**
+ * Расширенный класс соединения с базой данных.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
 namespace Evas\Db;
 
 use Evas\Db\BaseDatabase;
 use Evas\Db\Interfaces\DatabaseInterface;
 use Evas\Db\Traits\DatabaseQueryTrait;
+use Evas\Db\Traits\DatabaseTablesTrait;
 
 class Database extends BaseDatabase implements DatabaseInterface
 {
     // расширенные запросы БД
     use DatabaseQueryTrait;
     // таблицы БД
+    use DatabaseTablesTrait;
 }
