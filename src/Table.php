89c30836b9cc9ccce941224a9b9f1033c933f88d
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-09-19 05:32:07 +0300

    Create Table.php

---
+++ src/Table.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Класс таблицы базы данных.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db;
+
+use Evas\Db\Interfaces\TableInterface;
+
+class Table implements TableInterface
+{
+    // 
+}
