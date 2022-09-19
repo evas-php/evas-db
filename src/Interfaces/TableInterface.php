4665794bf8f7a6eaa056b299460b3c3fe686c05f
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-09-19 05:31:58 +0300

    Create TableInterface.php

---
+++ src/Interfaces/TableInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Интерфейс таблицы базы данных.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Interfaces;
+
+interface TableInterface
+{
+    // 
+}
