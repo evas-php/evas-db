0e965e3980b38ee0ce6850e5e22ad9a1871e4139
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-09-19 05:32:21 +0300

    Create DatabaseTablesTrait.php

---
+++ src/Traits/DatabaseTablesTrait.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Трейт таблиц базы данных.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\Traits;
+
+use Evas\Db\Table;
+
+trait DatabaseTablesTrait
+{
+    /** @var array список таблиц */
+    protected $tablesList;
+    /** @var array маппинг объектов таблиц */
+    protected $tableObjects = [];
+
+    /**
+     * Получение списка таблиц базы данных.
+     * @param bool перезапросить список таблиц
+     * @return array|null
+     */
+    public function tablesList(bool $reload = false): ?array
+    {
+        if (null === $this->tablesList || true === $reload) {
+            $this->tablesList = $this->grammar()->getTablesList();
+        }
+        return $this->tablesList;
+    }
+
+    /**
+     * Получение объекта таблицы.
+     * @param string имя таблицы
+     * @return Table
+     */
+    public function table(string $table): Table
+    {
+        $table = $this->grammar()->unwrapTable($table);
+        if (empty($this->tableObjects[$table])) {
+            $this->tableObjects[$table] = new Table($this, $table);
+        }
+        return $this->tableObjects[$table];
+    }
+}
