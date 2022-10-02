27521bc7e8398dd54a391e06324d3d3a7582fd2e
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-02 22:14:19 +0300

    Update Database.php

--- src/Database.php
+++ src/Database.php
@@ -9,12 +9,15 @@ namespace Evas\Db;
 use Evas\Db\BaseDatabase;
 use Evas\Db\Interfaces\DatabaseInterface;
 use Evas\Db\Traits\DatabaseQueryTrait;
+use Evas\Db\Traits\DatabaseSchemaCacheTrait;
 use Evas\Db\Traits\DatabaseTablesTrait;
 
 class Database extends BaseDatabase implements DatabaseInterface
 {
     // расширенные запросы БД
     use DatabaseQueryTrait;
+    // кэш схемы БД
+    use DatabaseSchemaCacheTrait;
     // таблицы БД
     use DatabaseTablesTrait;
 }
