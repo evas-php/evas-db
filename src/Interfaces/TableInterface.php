85253975fef9ec2353a71fe6b615d148578ff973
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-03 01:36:01 +0300

    Update TableInterface.php

--- src/Interfaces/TableInterface.php
+++ src/Interfaces/TableInterface.php
@@ -8,5 +8,35 @@ namespace Evas\Db\Interfaces;
 
 interface TableInterface
 {
-    // 
+    /**
+     * Получение первичного ключа таблицы.
+     * @return string
+     */
+    public function primaryKey(): string;
+
+    /**
+     * Получение столбцов таблицы.
+     * @return array
+     */
+    public function columns(): array;
+
+    /**
+     * Получение внешних ключей таблицы.
+     * @return array
+     */
+    public function foreignKeys(): array;
+
+    /**
+     * Получение id последней вставленной записи.
+     * @return int
+     */
+    public function lastInsertId(): int;
+
+    /**
+     * Проброс методов сборщика запросов через магию php.
+     * @param string имя метода
+     * @param array|null аргументы
+     * @return mixed
+     */
+    public function __call(string $name, array $args = null);
 }
