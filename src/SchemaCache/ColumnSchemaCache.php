3077d7171675b17d97f8d7f0bc762b9c9a68cda1
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-03 01:29:35 +0300

    Create ColumnSchemaCache.php

---
+++ src/SchemaCache/ColumnSchemaCache.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Кэш схемы колонки.
+ * @package evas-php\evas-db
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Db\SchemaCache;
+
+class ColumnSchemaCache
+{
+    /** @var string имя */
+    public $name;
+    /** @var string тип */
+    public $type;
+    /** @var bool разрешен ли NULL */
+    public $null;
+    /** @var string|null ключ */
+    public $key;
+    /** @var string|null значение по умолчанию */
+    public $default;
+    /** @var string|null extra */
+    public $extra;
+
+    /**
+     * Конструктор.
+     * @param array параметры колонки
+     */
+    public function __construct(array $params = null)
+    {
+        if ($params) foreach ($params as $name => $value) {
+            $this->__set($name, $value);
+        }
+    }
+
+    public function __set(string $name, $value)
+    {
+        if ('Field' === $name) $name = 'name';
+        else if ('Null' === $name) $value = 'YES' == $value ? true : false;
+        $name = strtolower($name);
+        $this->$name = $value;
+    }
+
+    /**
+     * Проверка на индекс.
+     * @return bool
+     */
+    public function isIndex(): bool
+    {
+        return !empty($this->key) ? true : false;
+    }
+
+    /**
+     * Приведение к строке.
+     */
+    public function __toString()
+    {
+        return json_encode($this);
+    }
+}
