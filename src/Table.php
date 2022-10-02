b368758dd7207324688c6ecb19ce16c1a4fa92a4
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-03 01:42:38 +0300

    Update Table.php

--- src/Table.php
+++ src/Table.php
@@ -7,8 +7,23 @@
 namespace Evas\Db;
 
 use Evas\Db\Interfaces\TableInterface;
+use Evas\Db\SchemaCache\TableSchemaCache;
 
-class Table implements TableInterface
+class Table extends TableSchemaCache implements TableInterface
 {
-    // 
+    /**
+     * Получение id последней вставленной записи.
+     * @return int
+     */
+    public function lastInsertId(): int
+    {}
+
+    /**
+     * Проброс методов сборщика запросов через магию php.
+     * @param string имя метода
+     * @param array|null аргументы
+     * @return mixed
+     */
+    public function __call(string $name, array $args = null)
+    {}
 }
