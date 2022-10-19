b34374f2b9c89bacf866d3d12419cf52b69c0888
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-19 14:54:45 +0300

    Update QueryBuilder.php

--- src/Builders/QueryBuilder.php
+++ src/Builders/QueryBuilder.php
@@ -6,166 +6,87 @@
  */
 namespace Evas\Db\Builders;
 
-use Evas\Db\Interfaces\DatabaseInterface;
 use Evas\Db\Interfaces\QueryBuilderInterface;
 
-class QueryBuilder implements QueryBuilderInterface
+use Evas\Db\Builders\AbstractQueryBuilder;
+
+use Evas\Db\Builders\Traits\SubQueryTrait;
+use Evas\Db\Builders\Traits\WrapsTrait;
+use Evas\Db\Builders\Traits\BindingsTrait;
+
+use Evas\Db\Builders\Traits\AggregatesTrait;
+use Evas\Db\Builders\Traits\SelectTrait;
+use Evas\Db\Builders\Traits\FromTrait;
+
+use Evas\Db\Builders\Traits\JoinsTrait;
+use Evas\Db\Builders\Traits\WhereTrait;
+use Evas\Db\Builders\Traits\GroupByTrait;
+use Evas\Db\Builders\Traits\HavingTrait;
+use Evas\Db\Builders\Traits\OrderByTrait;
+
+class QueryBuilder extends AbstractQueryBuilder implements QueryBuilderInterface
 {
-    /** @var DatabaseInterface соединение с базой данных */
-    public $db;
+    use SubQueryTrait;
+    use WrapsTrait;
+    use BindingsTrait;
+
+    use AggregatesTrait;
+    use SelectTrait;
+    use FromTrait;
+
+    use JoinsTrait;
+    use WhereTrait;
+    use GroupByTrait;
+    use HavingTrait;
+    use OrderByTrait;
+
+
+    public $type = 'select';
+
+    // public $from = [];
+    // public $columns = [];
+    // public $joins = [];
+    // public $wheres = [];
+    // public $groups = [];
+    // public $havings = [];
+    // public $orders = [];
+    
+    public $limit;
+    public $offset;
+
+    public $unions = [];
+    
+    // protected $bindings = [
+    //     'from' => [],
+    //     'update' => [],
+    //     'columns' => [],
+    //     'joins' => [],
+    //     'wheres' => [],
+    //     'havings' => [],
+    //     'unions' => [],
+    // ];
+
 
     /**
-     * Конструктор.
-     * @param DatabaseInterface соединение с базой данных
-     * @param string|null имя таблицы
+     * Установка лимита.
+     * @param int|null лимит или null для сброса
+     * @param int|null сдвиг или null для сброса
+     * @return self
      */
-    public function __construct(DatabaseInterface &$db, string $table = null)
+    public function limit(int $limit = null, int $offset = null)
     {
-        $this->db = $db;
-        if (!is_null($table)) $this->from($table);
-    }
-
-
-    // Получение данных для выполнения запроса
-
-    /**
-     * Получение собранного sql-запроса.
-     * @return string
-     */
-    public function getSql(): string
-    {
-        return $this->db->grammar()->buildQuery($this);
-    }
-
-    /**
-     * Получение экранируемых значений собранного запроса.
-     * @param string|null для получения части экранируемых значений
-     * @return array
-     */
-    public function getBindings(string $part = null): array
-    {
-        return [];
-    }
-
-    /**
-     * Получение sql-запроса и экранируемых значений.
-     * @return array [sql, values]
-     */
-    public function getSqlAndBindings(): array
-    {
-        return [$this->getSql(), $this->getBindings()];
-    }
-
-
-    // Выполнение запроса
-
-    /**
-     * Выполнение sql-запроса.
-     * @return QueryResultInterface|object|array of objects
-     */
-    public function query() //: QueryResultInterface
-    {
-        return $this->db->query($this->getSql(), $this->getBindings());
+        $this->limit = $limit < 1 ? null : $limit;
+        return func_num_args() > 1 ? $this->offset($offset) : $this;
     }
 
     /**
-     * Выполнение select-запроса с получением нескольких записей.
-     * @param array|null столбцы для получения
-     * @return array найденные записи
+     * Установка сдвига.
+     * @param int|null сдвиг или null для сброса
+     * @return self
      */
-    public function get($columns = null): array
+    public function offset(int $offset = null)
     {
-        if ($columns) $this->addSelect(...func_get_args());
-        return $this->query()->assocArrayAll();
-    }
-
-     /**
-     * Выполнение select-запроса с получением одной записи.
-     * @param array|null столбцы для получения
-     * @return array|null найденная запись
-     */
-    public function one($columns = null)
-    {
-        if ($columns) $this->addSelect(...func_get_args());
-        return $this->limit(1)->query()->assocArray();
-    }
-
-    /**
-     * Поиск записи/записей по первичному ключу.
-     * @param array|numeric|string значение первичного ключа
-     * @param array|null запрашиваемые поля
-     * @return array найденная или найденные записи
-     * @throws \InvalidArgumentException
-     */
-    public function find($id, array $columns = ['*'])
-    {
-        $this->select($columns);
-        $primaryKey = $this->primaryKey();
-        if (is_array($id))
-            return $this->whereIn($primaryKey, $id)->get();
-        else if (is_numeric($id) || is_string($id))
-            return $this->where($primaryKey, $id)->one();
-        else throw new \InvalidArgumentException(sprintf(
-            'Argument 1 passed to %s() must be an array, a numeric or a string, %s given',
-            __METHOD__, gettype($id)
-        ));
-    }
-
-    /**
-     * Выолнение delete-запроса удаления записи/записей.
-     * @param mixed|null id записи/записей, если нужно удалить конкретные
-     * @return QueryResultInterface
-     * @throws \InvalidArgumentException
-     */
-    public function delete($id = null) //: QueryResultInterface
-    {
-        $this->type = 'delete';
-        if ($id) {
-            $primaryKey = $this->primaryKey();
-            $this->wheres = [];
-            $this->bindings['where'] = [];
-            if (is_array($id)) {
-                $this->whereIn($primaryKey, $id)->limit(count($id));
-            } else if (is_numeric($id) || is_string($id)) {
-                $this->where($primaryKey, $id)->limit(1);
-            } else {
-                throw new \InvalidArgumentException(sprintf(
-                    'Argument 1 passed to %s() must be an array, a numeric or a string, %s given',
-                    __METHOD__, gettype($id)
-                ));
-            }
-        }
-        return $this->query();
-    }
-
-    /**
-     * Выолнение update-запроса обновления записи/записей sql-строкой.
-     * @param string sql-запрос
-     * @param array обновлённые данные
-     * @return QueryResultInterface
-     */
-    public function updateRaw(string $sql, array $vals) //: QueryResultInterface
-    {
-        $this->type = 'update';
-        $this->updateSql = $sql;
-        $this->addBindings('update', $vals);
-        return $this->query();
-    }
-
-    /**
-     * Выолнение update-запроса обновления записи/записей.
-     * @param array обновлённые данные
-     * @return QueryResultInterface
-     */
-    public function update(array $data) //: QueryResultInterface
-    {
-        $vals = [];
-        $sql = [];
-        foreach ($data as $key => $val) {
-            $sql[] = $this->wrap($key) . ' = ?';
-            $vals[] = $this->db->quoteArrayOrObject($val);
-        }
-        $sql = implode(', ', $sql);
-        return $this->updateRaw($sql, $vals);
+        $this->offset = $offset < 1 ? null : $offset;
+        return $this;
     }
 }
