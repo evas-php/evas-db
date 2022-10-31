b9097c601bb4bfd918c1b35ec3fae65c30525f22
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-31 04:47:45 +0300

    Update WhereJsonTrait.php

--- src/Builders/Traits/WhereJsonTrait.php
+++ src/Builders/Traits/WhereJsonTrait.php
@@ -1,76 +1,74 @@
 <?php
 /**
- * Трейт сборки WHERE части json.
+ * @todo Трейт сборки WHERE части json.
  * @package evas-php\evas-db
  * @author Egor Vasyakin <egor@evas-php.com>
  */
 namespace Evas\Db\Builders\Traits;
 
-use Evas\Db\Builders\Options\WhereOption;
-
 trait WhereJsonTrait
 {
-    public function whereJsonContains(string $column, $value)
-    {
-        $this->wheres[] = WhereOption::jsonContains($column, $value, false, false);
-        return $this;
+    public function whereJsonContains(
+        string $column, $value, bool $isOr = false, bool $isNot = false
+    ) {
+        return $this->pushWhere('JsonContains', compact('column', 'value', 'isOr', 'isNot'));
     }
 
     public function orWhereJsonContains(string $column, $value)
     {
-        $this->wheres[] = WhereOption::jsonContains($column, $value, true, false);
-        return $this;
+        return $this->whereJsonContains($column, $value, true);
     }
 
     public function whereNotJsonContains(string $column, $value)
     {
-        $this->wheres[] = WhereOption::jsonContains($column, $value, false, true);
-        return $this;
+        return $this->whereJsonContains($column, $value, false, true);
     }
 
     public function orWhereNotJsonContains(string $column, $value)
     {
-        $this->wheres[] = WhereOption::jsonContains($column, $value, true, true);
-        return $this;
+        return $this->whereJsonContains($column, $value, true, true);
     }
 
 
 
-    public function whereJsonContainsPathAll(string $column, $value, bool $isOne = false)
-    {
-        $this->wheres[] = WhereOption::jsonContainsPathAll($column, $value, $isOne, false, false);
-        return $this;
+    public function whereJsonContainsPathAll(
+        string $column, array $paths, bool $isOne = false, bool $isOr = false, bool $isNot = false
+    ) {
+        return $this->pushWhere(
+            'JsonContainsPath', compact('column', 'paths', 'isOne', 'isOr', 'isNot')
+        );
     }
 
     public function orWhereJsonContainsPathAll(string $column, $value, bool $isOne = false)
     {
-        $this->wheres[] = WhereOption::jsonContainsPathAll($column, $value, $isOne, true, false);
-        return $this;
+        return $this->whereJsonContainsPathAll($column, $paths, false, true);
     }
 
     public function whereNotJsonContainsPathAll(string $column, $value, bool $isOne = false)
     {
-        $this->wheres[] = WhereOption::jsonContainsPathAll($column, $value, $isOne, false, true);
-        return $this;
+        return $this->whereJsonContainsPathAll($column, $paths, true);
     }
 
     public function orWhereNotJsonContainsPathAll(string $column, $value, bool $isOne = false)
     {
-        $this->wheres[] = WhereOption::jsonContainsPathAll($column, $value, $isOne, true, true);
-        return $this;
+        return $this->whereJsonContainsPathAll($column, $paths, true, true);
     }
 
 
 
+    protected function pushWhereJsonLength(bool $isOr, string $column, $operator, $value = null)
+    {
+        $this->prepareValueAndOperator($value, $operator, func_num_args() === 3);
+        return $this->pushWhere('JsonLength', compact('column', 'operator', 'value'));
+    }
+
     public function whereJsonLength(string $column, $operator, $value = null)
     {
-        $this->wheres[] = WhereOption::jsonLength(false, ...func_get_args());
-        return $this;
+        return $this->pushWhereJsonLength(false, ...func_get_args());
     }
 
     public function orWhereJsonLength(string $column, $operator, $value = null)
     {
-        $this->wheres[] = WhereOption::jsonLength(true, ...func_get_args());
-        return $this;
+        return $this->pushWhereJsonLength(true, ...func_get_args());
     }
 }
