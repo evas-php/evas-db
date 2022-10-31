fb60159f0d8bf56c05984eba8984badd3fd16a2b
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-31 04:48:52 +0300

    Update HavingTrait.php

--- src/Builders/Traits/HavingTrait.php
+++ src/Builders/Traits/HavingTrait.php
@@ -13,110 +13,180 @@ trait HavingTrait
     /** @var array HAVING часть */
     public $havings = [];
 
-    public function havingRaw(string $sql, array $bindings = null)
+
+    // Help methods
+
+    /**
+     * Добавление having условия в сборку.
+     * @param string тип having
+     * @param array параметры условия
+     * @return self
+     */
+    protected function pushHaving(string $type, array $having)
     {
-        $this->havings[] = HavingOption::raw($sql, $bindings);
+        $having['type'] = $type;
+        $this->havings[] = $having;
+        if (!empty($having['bindings'])) {
+            $this->addBindings('havings', $having['bindings']);
+        }
         return $this;
     }
 
-    public function orHavingRaw(string $sql, array $bindings = null)
+    protected function pushSingleHaving(bool $isOr, $column, $operator = null, $value = null)
     {
-        $this->havings[] = HavingOption::raw($sql, $bindings, true);
-        return $this;
+        // массив where условий
+        if (is_array($column)) {
+            return $this->eachSingle('pushSingleHaving', $column, $isOr);
+        }
+
+        // подготовка значения и оператора условия
+        static::prepareValueAndOperator($value, $operator, func_num_args() === 3);
+
+        // IS NULL условие
+        if (is_null($value)) return $this->havingNull($column, $isOr, $operator !== '=');
+
+        // single условие
+        $column = $this->prepareColumn($column, 'havings');
+        $bindings = [$value];
+        return $this->pushHaving('Single', compact('column', 'operator', 'bindings', 'isOr'));
     }
+
+    /**
+     * Вспомогательная функция для добавление having агрегации.
+     * @param bool использовать ли OR для склейки
+     * @param string функция агрегации
+     * @param string столбец агрегации
+     * @param mixed оператор или значение
+     * @param mixed|null значение или null
+     * @return self
+     */
+    protected function pushHavingAggregate(
+        bool $isOr, string $function, string $column, $operator, $value = null
+    ) {
+        static::prepareValueAndOperator($value, $operator, func_num_args() === 4);
+        $column = $this->getAggregateColumn($function, $column);
+        return $this->pushSingleHaving($isOr, $column, $operator, $value);
+    }
+
+
+    // ----------
+    // Or/And Having Raw
+    // ----------
+
+
+    public function havingRaw(string $sql, array $bindings = [], bool $isOr = false)
+    {
+        return $this->pushHaving('Raw', compact('sql', 'bindings', 'isOr'));
+    }
+
+    public function orHavingRaw(string $sql, array $bindings = [])
+    {
+        return $this->havingRaw($sql, $bindings);
+    }
+
+
+    // ----------
+    // Or/And Having
+    // ----------
 
 
     public function having($column, $operator = null, $value = null)
     {
-        $this->havings[] = HavingOption::single(false, ...func_get_args());
-        return $this;
+        return $this->pushSingleHaving(false, ...func_get_args());
     }
 
     public function orHaving($column, $operator = null, $value = null)
     {
-        $this->havings[] = HavingOption::single(true, ...func_get_args());
-        return $this;
+        return $this->pushSingleHaving(true, ...func_get_args());
     }
 
 
-    // Null
+    // ----------
+    // Or/And Having Is (Not) Null
+    // ----------
 
-    public function havingNull($column)
+    public function havingNull($column, bool $isOr = false, bool $isNot = false)
     {
-        $this->havings[] = HavingOption::null($column, false, false);
-        return $this;
+        if (is_array($column)) {
+            foreach ($column as $sub) {
+                $this->havingNull($sub, $isOr, $isNot);
+            }
+            return $this;
+        }
+        $column = $this->prepareColumn($column, 'havings');
+        return $this->pushHaving('Null', compact('column', 'isOr', 'isNot'));
     }
 
     public function orHavingNull($column)
     {
-        $this->havings[] = HavingOption::null($column, true, false);
-        return $this;
+        return $this->havingNull($column, true, false);
     }
 
     public function havingNotNull($column)
     {
-        $this->havings[] = HavingOption::null($column, false, true);
-        return $this;
+        return $this->havingNull($column, false, true);
     }
 
     public function orHavingNotNull($column)
     {
-        $this->havings[] = HavingOption::null($column, true, true);
-        return $this;
+        return $this->havingNull($column, true, true);
     }
 
 
-    // Between
+    // ----------
+    // Or/And Having (Not) In
+    // ----------
 
-    public function havingBetween($column, $values)
+    /**
+     * Добавление having AND/OR (NOT) IN.
+     * @param string|\Closure|self столбец или подзарос столбца
+     * @param array|\Closure|self массив значений или подзапрос
+     * @param bool|null использовать ли OR для склейки
+     * @param bool|null использовать ли NOT
+     * @return self
+     */
+    public function havingIn($column, $values, bool $isOr = false, bool $isNot = false)
     {
-        $this->havings[] = HavingOption::between($column, $values, false, false);
-        return $this;
+        $column = $this->prepareColumn($column);
+        if (is_array($values)) {
+            $bindings = $values;
+        } else {
+            [$values, $bindings] = $this->createSub($values);
+        }
+        return $this->pushHaving('In', compact('column', 'values', 'bindings', 'isOr', 'isNot'));
     }
 
-    public function orHavingBetween($column, $values)
+    /**
+     * Добавление having OR IN.
+     * @param string|\Closure|self столбец или подзарос столбца
+     * @param array|\Closure|self массив значений или подзапрос
+     * @return self
+     */
+    public function orHavingIn($column, $values)
     {
-        $this->havings[] = HavingOption::between($column, $values, true, false);
-        return $this;
+        return $this->whereIn($column, $values, true);
     }
 
-    public function havingNotBetween($column, $values)
+    /**
+     * Добавление having AND NOT IN.
+     * @param string|\Closure|self столбец или подзарос столбца
+     * @param array|\Closure|self массив значений или подзапрос
+     * @return self
+     */
+    public function havingNotIn($column, $values)
     {
-        $this->havings[] = HavingOption::between($column, $values, false, true);
-        return $this;
+        return $this->whereIn($column, $values,false, true);
     }
 
-    public function orHavingNotBetween($column, $values)
+    /**
+     * Добавление having OR NOT IN.
+     * @param string|\Closure|self столбец или подзарос столбца
+     * @param array|\Closure|self массив значений или подзапрос
+     * @return self
+     */
+    public function orHavingNotIn($column, $values)
     {
-        $this->havings[] = HavingOption::between($column, $values, true, true);
-        return $this;
-    }
-
-
-    // Between columns
-
-    public function havingBetweenColumns($column, $columns)
-    {
-        $this->havings[] = HavingOption::betweenColumns($column, $columns, false, false);
-        return $this;
-    }
-
-    public function orHavingBetweenColumns($column, $columns)
-    {
-        $this->havings[] = HavingOption::betweenColumns($column, $columns, true, false);
-        return $this;
-    }
-
-    public function havingNotBetweenColumns($column, $columns)
-    {
-        $this->havings[] = HavingOption::betweenColumns($column, $columns, false, true);
-        return $this;
-    }
-
-    public function orHavingNotBetweenColumns($column, $columns)
-    {
-        $this->havings[] = HavingOption::betweenColumns($column, $columns, true, true);
-        return $this;
+        return $this->whereIn($column, $values, true, true);
     }
 
 
@@ -124,13 +194,11 @@ trait HavingTrait
 
     public function havingAggregate(string $function, string $column, $operator, $value = null)
     {
-        $this->havings[] = HavingOption::aggregate(false, ...func_get_args());
-        return $this;
+        return $this->pushHavingAggregate(false, ...func_get_args());
     }
 
     public function orHavingAggregate(string $function, string $column, $operator, $value = null)
     {
-        $this->havings[] = HavingOption::aggregate(true, ...func_get_args());
-        return $this;
+        return $this->pushHavingAggregate(true, ...func_get_args());
     }
 }
