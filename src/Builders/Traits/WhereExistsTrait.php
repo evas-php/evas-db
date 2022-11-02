befc61c81744bff9c6f3120051dcb6047a2ac469
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-11-03 02:59:19 +0300

    Update WhereExistsTrait.php

--- src/Builders/Traits/WhereExistsTrait.php
+++ src/Builders/Traits/WhereExistsTrait.php
@@ -12,43 +12,48 @@ trait WhereExistsTrait
      * Добавление where exists.
      * По умолчанию склейка через AND.
      * @param string|\Closure|self подзапрос
+     * @param array|null экранируемые значения
      * @param bool использовать ли OR для склейки where
      * @param bool использовать ли NOT перед between
      * @return self
      */
-    public function whereExists($query, bool $isOr = false, bool $isNot = false)
-    {
-        [$sql, $bindings] = $this->createSub($query);
+    public function whereExists(
+        $query, array $bindings = [], bool $isOr = false, bool $isNot = false
+    ) {
+        [$sql, $bindings] = $this->createSub($query, $bindings);
         return $this->pushWhere('Exists', compact('sql', 'bindings', 'isOr', 'isNot'));
     }
 
     /**
      * Добавление where OR exists.
      * @param string|\Closure|self подзапрос
+     * @param array|null экранируемые значения
      * @return self
      */
-    public function orWhereExists($query)
+    public function orWhereExists($query, array $bindings = [])
     {
-        return $this->whereExists($query, true);
+        return $this->whereExists($query, $bindings, true);
     }
 
     /**
      * Добавление where AND NOT exists.
      * @param string|\Closure|self подзапрос
+     * @param array|null экранируемые значения
      * @return self
      */
-    public function whereNotExists($query)
+    public function whereNotExists($query, array $bindings = [])
     {
-        return $this->whereExists($query, false, true);
+        return $this->whereExists($query, $bindings, false, true);
     }
 
     /**
      * Добавление where OR NOT exists.
      * @param string|\Closure|self подзапрос
+     * @param array|null экранируемые значения
      * @return self
      */
-    public function orWhereNotExists($query)
+    public function orWhereNotExists($query, array $bindings = [])
     {
-        return $this->whereExists($query, true, true);
+        return $this->whereExists($query, $bindings, true, true);
     }
 }
