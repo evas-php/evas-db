997a92ec01582b60e440eac0c50f235f04d81350
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-11-03 04:39:52 +0300

    Update HavingBetweenTrait.php

--- src/Builders/Traits/HavingBetweenTrait.php
+++ src/Builders/Traits/HavingBetweenTrait.php
@@ -62,37 +62,4 @@ trait HavingBetweenTrait
     {
         return $this->havingBetween($column, $values, true, true);
     }
-
-
-    // Between columns
-
-    public function havingBetweenColumns(
-        $column, array $columns, bool $isOr = false, bool $isNot = false
-    ) {
-        $column = $this->prepareColumn($column, 'havings');
-        $columns = array_slice($columns, 0, 2);
-        if (count($columns) < 2) {
-            throw new \InvalidArgumentException(sprintf(
-                'Argument #2 (array $columns) passed to %s() must has 2 values',
-                __METHOD__
-            ));
-        }
-        $columns = array_map([$this, 'prepareColumn'], $columns);
-        return $this->pushHaving('BetweenColumns', compact('column', 'columns', 'isOr', 'isNot'));
-    }
-
-    public function orHavingBetweenColumns($column, array $columns)
-    {
-        return $this->havingBetweenColumns($column, $columns, true, false);
-    }
-
-    public function havingNotBetweenColumns($column, array $columns)
-    {
-        return $this->havingBetweenColumns($column, $columns, false, true);
-    }
-
-    public function orHavingNotBetweenColumns($column, array $columns)
-    {
-        return $this->havingBetweenColumns($column, $columns, true, true);
-    }
 }
