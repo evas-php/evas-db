fb26de463e6ac0b23874ef4811fc81f94184e942
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-11-03 05:09:39 +0300

    Update QueryBuilderInterface.php

--- src/Interfaces/QueryBuilderInterface.php
+++ src/Interfaces/QueryBuilderInterface.php
@@ -12,8 +12,27 @@ use Evas\Db\Interfaces\BaseQueryBuilderInterface;
 
 interface QueryBuilderInterface extends BaseQueryBuilderInterface
 {
-    
+    /**
+     * Установка from sql сторокой.
+     * @param string sql строка
+     * @param array|null экранируемые значения
+     * @return self
+     */
     public function fromRaw(string $sql, array $bindings = []);
 
+    /**
+     * Установка from таблицей или sql-подзапросом.
+     * @param string|\Closure|self таблица или подзапрос
+     * @param string|null псевдоним
+     * @return self
+     */
+    public function from($table, string $as = null);
+
+    /**
+     * Установка from sql-подзапросом.
+     * @param \Closure|self|string
+     * @param string псевдоним
+     * @return self
+     */
     public function fromSub($query, string $as);
 }
